6922a35d393d84a2ec8bbd9201007f5cbaa7c212
Rajiv aryal <rajivary1@gmail.com>
2024-05-09 13:35:54 +0545

    creating post type

--- public/class-plugin-name-public.php
+++ public/class-plugin-name-public.php
@@ -16,20 +16,20 @@
  * Defines the plugin name, version, and two examples hooks for how to
  * enqueue the public-facing stylesheet and JavaScript.
  *
- * @package    Plugin_Name
+ * @package    Rmail
  * @subpackage Plugin_Name/public
- * @author     Your Name <email@example.com>
+ * @author     <aryalrajiv class="com np"></aryalrajiv>
  */
-class Plugin_Name_Public {
+class Rmail_Public {
 
 	/**
 	 * The ID of this plugin.
 	 *
 	 * @since    1.0.0
 	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
+	 * @var      string    $Rmail    The ID of this plugin.
 	 */
-	private $plugin_name;
+	private $Rmail;
 
 	/**
 	 * The version of this plugin.
@@ -44,12 +44,12 @@ class Plugin_Name_Public {
 	 * Initialize the class and set its properties.
 	 *
 	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of the plugin.
+	 * @param      string    $Rmail       The name of the plugin.
 	 * @param      string    $version    The version of this plugin.
 	 */
-	public function __construct( $plugin_name, $version ) {
+	public function __construct( $Rmail, $version ) {
 
-		$this->plugin_name = $plugin_name;
+		$this->plugin_name = $Rmail;
 		$this->version = $version;
 
 	}
@@ -59,45 +59,23 @@ class Plugin_Name_Public {
 	 *
 	 * @since    1.0.0
 	 */
-	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Plugin_Name_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Plugin_Name_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/plugin-name-public.css', array(), $this->version, 'all' );
-
-	}
-
-	/**
-	 * Register the JavaScript for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Plugin_Name_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Plugin_Name_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/plugin-name-public.js', array( 'jquery' ), $this->version, false );
-
-	}
-
 }
+	class SimpleContactForm{
+
+       public function Contact_construct(){
+          add_action('init',array($this,'Create_custom_post_type'));
+
+	   }
+
+	   public function Create_custom_post_type(){
+           echo " <script> alert('Itloaded')</script>";
+	   }
+
+
+
+	}
+
+	new SimpleContactForm;
+
+
+
